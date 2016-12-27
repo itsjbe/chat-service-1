e1ca59084fc8e029098bdb378cd62e6be390feac
JBe <bednarik.net@gmail.com>
2016-12-27 18:21:36 +0100

    Project baseline.

---
+++ chat-service-1/src/chat-service-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace chat_service_1
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseUrls("http://+:5002");
+
+            var host = builder.Build();
+            host.Run();
+
+            return 0;
+        }
+    }
+}
---
+++ chat-service-1/src/chat-service-1/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace chat_service_1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+
+            app.UseStaticFiles();
+
+            app.Run(async context =>
+                    {
+                        context.Response.ContentType = "text/html";
+                        await context.Response
+                                     .WriteAsync("<p>Hosted by Kestrel</p>");
+
+                        if (serverAddressesFeature != null)
+                        {
+                            await context.Response
+                                         .WriteAsync("<p>Listening on the following addresses: " +
+                                                     string.Join(", ", serverAddressesFeature.Addresses) +
+                                                     "</p>");
+                        }
+
+                        await context.Response.WriteAsync($"<p>Request URL: {context.Request.GetDisplayUrl()}<p>");
+                    });
+        }
+    }
+}
